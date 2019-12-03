e44cd8b45381ac124b904e7cc58e375331d024c4
Michael Prentice <splaktar@gmail.com>
2019-12-03 13:42:00 -0800

    fix(e2e): fix integration tests
    override webdriver-manager version of protractor
     - to a newer version that supports Chrome 78

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to dev-demo!');
+    expect(page.getParagraphText()).toEqual('DevIntent Demo App');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('dev-root h1')).getText();
+    return element(by.css('dev-root .mat-toolbar a')).getText();
   }
 }
